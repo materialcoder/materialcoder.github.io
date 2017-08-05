142d0005a39f9b009608737f1a64e26c26874611
materialcoder <1028106985@qq.com>
2017-08-05 19:03:31 +0800

    add loading

---
+++ js/loading.js
@@ -0,0 +1,19 @@
+var $ = function(e) {
+	return document.querySelectorAll(e);
+};
+var imgs = $('img');
+var num = 0;
+var len = imgs.length;
+var isScroll = false;
+imgs.forEach(function(item, index) {
+	var oImg = new Image();
+	oImg.onload = function() {
+		num++;
+		$(".loading b")[0].innerHTML = parseInt(num/len * 100) + "%";
+		if(num > len) {
+			$('.loading')[0].style.display = "none";
+			isScroll = true;
+		}
+	}
+	oImg.src = item.src
+});
